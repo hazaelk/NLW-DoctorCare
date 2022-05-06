b76ada04762c9158747b339a820d7923dd2b517d
Willian Santana <will10102011@gmail.com>
2022-05-06 19:18:57 -0300

    responsividade doctorcare nlw rocketseat

--- main.js
+++ main.js
@@ -6,8 +6,47 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
 }
 
+function activateMenuAtCurrentSection(section) {
+  // linha alvo
+  const targetLine = scrollY + innerHeight / 2
+
+  // verificar se a seção passou da linha
+  // quais dados vou precisar?
+
+  //o topo da seção
+  const sectionTop = section.offsetTop
+
+  // a altura da seção
+  const sectionHeight = section.offsetHeight
+
+  // verificar se o topo passou da linha alvo
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  // verificar se a base passou da linha alvo
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  //o final da seção passou da linha alvo
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  // limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  const sectionID = section.getAttribute('id')
+  var menuElement = document.querySelector(`.menu a[href*=${sectionID}`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+}
 function showNavOnScroll() {
   if (scrollY > 0) {
     nav.classList.add('scroll')
